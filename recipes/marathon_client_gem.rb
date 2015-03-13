0320056c47496852dd28f550878441afd7ca3434
Uwe Stuehler <us@hubrick.com>
2015-03-13 16:38:36 +0100

    Try a workaround for using chef_gem with local files in OpsWorks

--- recipes/marathon_client_gem.rb
+++ recipes/marathon_client_gem.rb
@@ -4,8 +4,11 @@ cookbook_file "#{Chef::Config[:file_cache_path]}/marathon_client-0.3.0.gem" do
 end.run_action(:create_if_missing)
 
 chef_gem 'marathon_client' do
-  source "#{Chef::Config[:file_cache_path]}/marathon_client-0.3.0.gem"
-  action :upgrade
-end
+  #source "#{Chef::Config[:file_cache_path]}/marathon_client-0.3.0.gem"
+  # XXX: http://stackoverflow.com/questions/27258938/install-gem-from-s3-in-opsworks-chef-recipe
+  action :nothing
+  version '0.3.0'
+  options "--local #{Chef::Config[:file_cache_path]}/marathon_client-0.3.0.gem"
+end.run_action(:upgrade)
 
 require 'marathon'
